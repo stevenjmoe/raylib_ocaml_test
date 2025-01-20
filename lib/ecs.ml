d1d567383a91f7ec84b30ae18b343ca87532be8e
Steven Moe <dev@stevenm.co>
2025-01-21 06:11:02 +1100

    Scrap almost everything and setup a very light Ecs module

---
+++ lib/ecs.ml
@@ -0,0 +1,137 @@
+module Entity = struct
+  type t = int
+end
+
+(* Components *)
+type position = { mutable x : float; mutable y : float; w : float; h : float }
+type velocity = { mutable vx : float; mutable vy : float }
+type animation_kind = Idle | Move | Attack
+type frame_info = { src_x : float; src_y : float; duration : float }
+
+type animation = {
+  mutable current_kind : animation_kind;
+  mutable current_frame : int;
+  mutable timer : float;
+  frames_idle : frame_info array;
+  texture : Raylib.Texture.t;
+}
+
+type world = {
+  last_id : int;
+  positions : (int, position) Hashtbl.t;
+  velocities : (int, velocity) Hashtbl.t;
+  animations : (int, animation) Hashtbl.t;
+}
+
+let create_world =
+  {
+    last_id = 0;
+    positions = Hashtbl.create 100;
+    velocities = Hashtbl.create 100;
+    animations = Hashtbl.create 100;
+  }
+
+(** [create_entity world] creates a new [entity] the the [world]'s [last_id]. It
+    then returns [new_world * entity] *)
+let create_entity (world : world) =
+  let entity = world.last_id in
+  let new_world = { world with last_id = entity + 1 } in
+  (new_world, entity)
+
+(** [add_position entity pos world] adds [pos] to [world.positions] and returns
+    [world] *)
+let add_position (entity : Entity.t) (pos : position) (world : world) =
+  Hashtbl.add world.positions entity pos;
+  world
+
+(** [add_velocity entity vel world] adds [vel] to [world.velocities] and returns
+    [world] *)
+let add_velocity (entity : Entity.t) (vel : velocity) (world : world) =
+  Hashtbl.add world.velocities entity vel;
+  world
+
+(** [add_animation entity anim world] adds [anim] to [world.animations] and
+    returns [world] *)
+let add_animation (entity : Entity.t) (anim : animation) (world : world) =
+  Hashtbl.add world.animations entity anim;
+  world
+
+let create_player (w : world) =
+  let open Raylib in
+  let w, player = create_entity w in
+  let pos = { x = 200.0; y = 200.0; w = 100.0; h = 100.0 } in
+  let vel = { vx = 0.0; vy = 0.0 } in
+
+  let player_idle_texture =
+    load_texture
+      "assets/Asset Packs 1-5/Asset Pack-V1/Player Idle/Player Idle 48x48.png"
+  in
+
+  let frames_idle =
+    [|
+      { src_x = 0.0; src_y = 0.0; duration = 0.2 };
+      { src_x = 48.0; src_y = 0.0; duration = 0.2 };
+      { src_x = 96.0; src_y = 0.0; duration = 0.2 };
+      { src_x = 144.0; src_y = 0.0; duration = 0.2 };
+      { src_x = 192.0; src_y = 0.0; duration = 0.2 };
+      { src_x = 240.0; src_y = 0.0; duration = 0.2 };
+      { src_x = 288.0; src_y = 0.0; duration = 0.2 };
+      { src_x = 336.0; src_y = 0.0; duration = 0.2 };
+      { src_x = 384.0; src_y = 0.0; duration = 0.2 };
+      { src_x = 432.0; src_y = 0.0; duration = 0.2 };
+    |]
+  in
+
+  let anim =
+    {
+      current_kind = Idle;
+      current_frame = 0;
+      timer = 0.0;
+      frames_idle;
+      texture = player_idle_texture;
+    }
+  in
+
+  let w =
+    w |> add_position player pos |> add_velocity player vel
+    |> add_animation player anim
+  in
+  (w, player)
+
+(* TODO: move to module *)
+let animation_system (dt : float) (w : world) =
+  Hashtbl.iter
+    (fun ent anim ->
+      match
+        (Hashtbl.find_opt w.velocities ent, Hashtbl.find_opt w.positions ent)
+      with
+      | Some _vel, Some _pos ->
+          (* TODO: handle other animations *)
+          let frames = anim.frames_idle in
+          anim.timer <- anim.timer +. dt;
+          let current_frame_duration = frames.(anim.current_frame).duration in
+          if anim.timer > current_frame_duration then (
+            anim.timer <- anim.timer -. current_frame_duration;
+            anim.current_frame <-
+              (anim.current_frame + 1) mod Array.length frames)
+      | _ -> ())
+    w.animations;
+  w
+
+(* Function to draw the sprite based on position and current animation frame *)
+let draw_sprite (entity : Entity.t) (w : world) =
+  match
+    (Hashtbl.find_opt w.positions entity, Hashtbl.find_opt w.animations entity)
+  with
+  | Some pos, Some anim ->
+      let frames = anim.frames_idle in
+      (* Only idle frames are defined *)
+      let frame = frames.(anim.current_frame) in
+      let src_rect =
+        Raylib.Rectangle.create frame.src_x frame.src_y 48.0 48.0
+      in
+      let dst_rect = Raylib.Rectangle.create pos.x pos.y pos.w pos.h in
+      Raylib.draw_texture_pro anim.texture src_rect dst_rect
+        (Raylib.Vector2.create 0.0 0.0)
+        0.0 Raylib.Color.white
+  | _ -> ()
--- lib/game.ml
+++ lib/game.ml
@@ -1,99 +1,32 @@
-open Raylib
+open Ecs
 
-let screen_width = 800
-let screen_height = 450
+let rec game_loop (w : world) (player : Entity.t) =
+  let open Raylib in
+  if Raylib.window_should_close () then (
+    Raylib.close_window ();
+    Raylib.close_audio_device ())
+  else
+    let dt = Raylib.get_frame_time () in
 
-module Sprite = struct
-  type t = {
-    texture : Texture2D.t;
-    x : float;
-    y : float;
-    width : float;
-    height : float;
-    velocity : Vector2.t;
-  }
+    (* Update systems *)
+    let w = animation_system dt w in
 
-  (** [set_velocity sprite vx vy] returns a new sprite with [velocity] updated
-      with [vx] and [vy] *)
-  let set_velocity sprite vx vy =
-    { sprite with velocity = Vector2.create vx vy }
+    (* Rendering *)
+    Raylib.begin_drawing ();
+    Raylib.clear_background Color.blue;
+    Raylib.draw_text "Idle Character Example" 10 10 20 Color.white;
+    draw_sprite player w;
+    Raylib.end_drawing ();
 
-  (** [apply_velocity sprite] returns a new sprite with the [x] and [y] values
-      updated with the values from [sprite.velocity] and the elapsed time frame.
-  *)
-  let apply_velocity sprite =
-    let dt = get_frame_time () in
-    let x = sprite.x +. (Vector2.x sprite.velocity *. dt) in
-    let y = sprite.y +. (Vector2.y sprite.velocity *. dt) in
-    { sprite with x; y }
-
-  (* TODO: This makes assumptions about the sprite being drawn that might not always be true. I can fix this later when it is needed. *)
-  let draw sprite =
-    let src = Rectangle.create 0.0 0.0 48.0 48.0 in
-    let dst = Rectangle.create sprite.x sprite.y sprite.width sprite.height in
-    draw_texture_pro sprite.texture src dst (Vector2.create 0.0 0.0) 0.0
-      Color.white
-end
-
-let move_player sprite =
-  let sprite =
-    if is_key_down Key.D then Sprite.set_velocity sprite 100.0 0.0
-    else if is_key_down Key.A then Sprite.set_velocity sprite (-100.0) 0.0
-    else Sprite.set_velocity sprite 0.0 0.0
-  in
-  sprite
-
-let apply_gravity (sprite : Sprite.t) =
-  let velocity =
-    match Vector2.y sprite.velocity +. 10.0 with
-    | v when v > 200.0 -> 200.0
-    | v -> v
-  in
-  { sprite with velocity = Vector2.create (Vector2.x sprite.velocity) velocity }
-
-let apply_vel sprite = Sprite.apply_velocity sprite
+    game_loop w player
 
 let setup () =
-  init_window screen_width screen_height "test game";
-  init_audio_device ();
+  Raylib.init_window 800 600 "Test game";
+  let world = Ecs.create_world in
+  let world, player = create_player world in
+  Raylib.set_target_fps 60;
+  (world, player)
 
-  let player_idle_texture =
-    load_texture
-      "assets/Asset Packs 1-5/Asset Pack-V1/Player Idle/Player Idle 48x48.png"
-  in
-
-  let player =
-    Sprite.
-      {
-        texture = player_idle_texture;
-        x = 200.0;
-        y = 100.0;
-        width = 100.0;
-        height = 100.0;
-        velocity = Vector2.create 0.0 0.0;
-      }
-  in
-  let _ =
-    Camera2D.create (Vector2.create 0.0 0.0)
-      (Vector2.create
-         (float_of_int (screen_width + 2))
-         (float_of_int (screen_height + 2)))
-      0.0 1.0
-  in
-  set_target_fps 60;
-  player
-
-let rec loop (player : Sprite.t) =
-  if Raylib.window_should_close () then (
-    close_window ();
-    close_audio_device ())
-  else
-    let player = player |> move_player |> apply_gravity |> apply_vel in
-    begin_drawing ();
-    clear_background Color.blue;
-    draw_text "A window! Amazing!" 190 200 20 Color.lightgray;
-    Sprite.draw player;
-    end_drawing ();
-    loop player
-
-let run = setup () |> loop
+let run =
+  let world, player = setup () in
+  game_loop world player
